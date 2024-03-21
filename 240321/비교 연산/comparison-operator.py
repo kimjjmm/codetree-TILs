2a89f4095d9e3181be49201742f30d2bdcc5b191
kimjjmm <114631300+kimjjmm@users.noreply.github.com>
2024-03-21 16:23:36 +0900

    Title: 비교 연산, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240321/비교 연산/comparison-operator.py
@@ -0,0 +1,11 @@
+inp = input().split()
+
+a = int(inp[0])
+b = int(inp[1])
+
+print(1 if a>=b else 0)
+print(1 if a>b else 0)
+print(1 if a<=b else 0)
+print(1 if a<b else 0)
+print(1 if a==b else 0)
+print(1 if a!=b else 0)
