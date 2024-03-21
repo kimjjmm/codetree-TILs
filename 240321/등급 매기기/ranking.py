079ac025ef6240a5b2180d0902b8d6f4b9a885ac
kimjjmm <114631300+kimjjmm@users.noreply.github.com>
2024-03-21 15:54:40 +0900

    Title: 등급 매기기, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240321/등급 매기기/ranking.py
@@ -0,0 +1,12 @@
+inp = int(input())
+
+if inp >= 90:
+    print('A')
+elif inp >= 80:
+    print('B')
+elif inp >= 70:
+    print('C')
+elif inp >= 60:
+    print('D')
+elif inp < 60:
+    print('F')
