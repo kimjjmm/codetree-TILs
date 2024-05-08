8f3090977d1e1a7520d770d655ec1ff5046c439f
kimjjmm <114631300+kimjjmm@users.noreply.github.com>
2024-05-08 16:48:27 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240508/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int,input().split()))
+sum_val = 0
+arr_even = []
+
+for elem in arr:
+    if elem % 2 == 0:
+        arr_even.append(elem)
+
+
+for value in reversed(arr_even):
+    print(value)
