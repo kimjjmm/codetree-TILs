718db3631676a7d0a232083c799b6b361376d758
kimjjmm <114631300+kimjjmm@users.noreply.github.com>
2024-05-08 17:12:29 +0900

    Title: 학점 계산기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240508/학점 계산기/credit-calculator.py
@@ -0,0 +1,15 @@
+n = int(input())
+sum_val = 0
+avg = 0
+arr = list(map(float, input().split()))
+
+for elem in arr:
+    sum_val += elem
+avg = sum_val/4
+print(f"{avg:.1f}")
+if avg >= 4.0:
+    print('Perfect')
+elif avg >= 3.0:
+    print("Good")
+else:
+    print("Poor")
